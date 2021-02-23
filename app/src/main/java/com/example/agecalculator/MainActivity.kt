be30c1f7222ee306c2f8c8bd5abc2b994a73b31c
Mngatia <nyawirahngatia@gmail.com>
2021-02-23 20:20:24 +0300

    Age In Minutes Calculator.

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.agecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker = findViewById(R.id.btnDatePicker) as Button
+        btnDatePicker.setOnClickListener { 
+            Toast.makeText(this, "Button works", Toast.LENGTH_LONG).show()
+        }
     }
 }
